6f4941b4e612ac25b4a71964e326c95ecd145e4b
jcao91 <jcao91@yahoo.com>
2024-05-15 18:21:14 -0500

    Steps 1: Defining Variables (let) and Steps 2: Storing Cache Elements References"

--- js/app.js
+++ js/app.js
@@ -4,13 +4,55 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board;
+// represents the state of the squares on the board
+
+let turn;
+// track whose turn it is
+
+let winner;
+
+let tie;
 
 
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelectorAll('.sqr');
+// console.log(squareEls);
+
+const messageEl = document.querySelector('#message');
+// console.log(messageEl);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
